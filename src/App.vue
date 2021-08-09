ec1740098802a1638f1cd712c80245e18b9b3e91
mel0ndev <86430838+mel0ndev@users.noreply.github.com>
2021-08-09 17:11:01 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+	<v-app id="app">
+	<v-app-bar
+		app
+		class="appBarClass"
+		dark>
+
+	<div class="d-flex align-center">
+		<v-img class="logo"
+		src="./assets/flame.png"
+		height="75"
+		width="75">
+		</v-img>
+		<span>
+			<h1 class="unihot"> Unihot </h1>
+		</span>
+		<br>
+			<span class="UnihotSubtitle"> <h4> The hottest coins on Uniswap </h4> </span>
+	
+	</div>
+
+	<v-spacer></v-spacer>
+
+	
+	</v-app-bar>
+
+
+		<v-main> 
+
+		<div class="table-wrapper">
+		
+		<Table> </Table>
+
+		</div>
+
+		</v-main>
+
+	</v-app>
+</template>
+
+
+<script>
+
+import Table from "./components/Table.vue"; 
+
+
+export default {
+
+	components: {
+		Table, 
+	}
+
+
+
+}
+
+</script>
+
+
+
+<style>
+
+#app {
+	background-color: #0d0d0d;
+	font-family: Roboto Mono, sans-serif;
+}
+
+
+.logo {
+	padding: 10px;
+	margin: 10px;
+}
+
+.unihot {
+	font-family: Roboto Mono, sans-serif;
+	font-size: 40px;
+	display:  inline-block;
+}
+
+.UnihotSubtitle {
+	margin: 10px;
+	padding-left: 10px;
+	padding-top: 15px;
+}
+
+.table-wrapper {
+	padding: 25px;
+}
+
+
+</style>
