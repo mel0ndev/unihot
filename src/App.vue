a566fbb8ffc33a4715f604eb768c6d39cab6f66a
mel0ndev <86430838+mel0ndev@users.noreply.github.com>
2021-08-18 19:12:16 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
 	<v-app id="app">
+
+		<v-navigation-drawer
+		v-model="drawer"
+		app
+		color="#282828"
+		>
+	<v-list-item class="itemlist">
+				<v-list-item-content class="content">
+					<v-list-item-title class="title">
+							Dashboard
+					</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</v-navigation-drawer>
 	<v-app-bar
 		app
 		class="appBarClass"
 		dark>
+
+		<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
 	<div class="d-flex align-center">
 		<v-img class="logo"
@@ -23,8 +39,16 @@
 	
 	</v-app-bar>
 
-
 		<v-main> 
+
+
+			<div class="extraSettings"> 
+
+
+				<ExtraSettings @changeDenom="logDenomChange" @changeLookBack="logLookBackChange"> </ExtraSettings>
+				<!-- COMPONENT GOES HERE -->
+
+			</div>
 
 		<div class="table-wrapper">
 		
@@ -40,14 +64,31 @@
 
 <script>
 
-import Table from "./components/Table.vue"; 
+import Table from "./components/Table.vue";
+import ExtraSettings from "./components/ExtraSettings.vue"; 
 
 
 export default {
 
 	components: {
 		Table, 
+		ExtraSettings,
+	},
+
+
+	data: () => ({ drawer: false }),
+
+
+	methods: {
+		logDenomChange(e) {
+		console.log(e); 
+		},
+
+		logLookBackChange(e) {
+			console.log(e); 
+		}
 	}
+
 
 
 
@@ -61,9 +102,12 @@ export default {
 
 #app {
 	background-color: #0d0d0d;
-	font-family: Roboto Mono, sans-serif;
+	font-family: Roboto monospace, sans-serif;
 }
 
+.title {
+	color: white;
+}
 
 .logo {
 	padding: 10px;
@@ -74,6 +118,13 @@ export default {
 	font-family: Roboto Mono, sans-serif;
 	font-size: 40px;
 	display:  inline-block;
+	background-image: linear-gradient(45deg, #FF6B8E, #6BFFDC);
+	background-size: 100%;
+	background-repeat: repeat;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent; 
+	-moz-background-clip: text;
+	-moz-text-fill-color: transparent;
 }
 
 .UnihotSubtitle {
@@ -81,6 +132,11 @@ export default {
 	padding-left: 10px;
 	padding-top: 15px;
 	color: rgba(255, 255, 255, 0.5);
+}
+
+
+.extraSettings {
+	padding: 50px;
 }
 
 .table-wrapper {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
+
 Vue.config.productionTip = false
+
+export const eventBus = new Vue(); 
+
+Vue.use(VueRouter);
+
 
 new Vue({
   vuetify,
